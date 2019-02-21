ed1126f50f78b483ee2932b913b37646845d62ec
MiqueiasFernandes <contatomiqueiasfernandes@hotmail.com>
2019-02-20 22:49:41 -0300

    Add files via upload

---
+++ caze_download.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+FAM=${1:-AA1_eukaryota}
+PAGS=${2:-33}
+
+
+echo "baixando os IDS..."
+
+for page in $(seq 0 $PAGS)
+	do 
+	page=$page"00"
+	wget "http://www.cazy.org/$FAM.html?debut_TAXO=$page#pagination_TAXO" -qO $page &
+done
+
+wait
+
+cat * | grep ncbi.nlm.nih.gov/entrez | cut -d= -f5- | cut -d\  -f1 >> links.txt
+
+split -dl190 links.txt part
+
+echo "baixando do NCBI..."
+
+for arquivo in $(ls part* | tr \s \\n)
+	do
+		ids=$(cat $arquivo | tr \\n , | cut -d, -f-200)
+		list=${ids: :-1}
+		wget "http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&rettype=fasta&id=$list" -qO fasta.$arquivo
+done
+
+
+
+cat fasta.* | grep -vP "^$" > $FAM.fa
+
+echo "os arquivos foram salvos em $FAM.fa !"
+
