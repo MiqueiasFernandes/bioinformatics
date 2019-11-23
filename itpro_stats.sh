57caf941fbfa554706134bc6f830fb07187d8e2d
MiqueiasFernandes <contatomiqueiasfernandes@hotmail.com>
2019-11-22 21:11:16 -0300

    Update itpro_stats.sh

--- itpro_stats.sh
+++ itpro_stats.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## https://github.com/ebi-pf-team/interproscan/wiki/OutputFormats
+
 mkdir tmp
 
 FILE=$1
@@ -7,7 +9,7 @@ cp $FILE tmp/main.tsv
 FILE=tmp/main.tsv
 OUT=$FILE.ptnas.anot
 
-cut -f1,14 $FILE  | grep GO | cut -f1 | sort -u > $OUT.go
+cut -f1,14 $FILE  | grep GO | cut -f1 | sort -u > $OUT.GO
 cut -f1,12 $FILE  | grep IPR  | cut -f1 | sort -u > $OUT.ipro
 cut -d\; -f1 $FILE  | sort -u > $OUT.TOTAL
 cut -f4 $FILE  | sort -u > dbs
@@ -20,7 +22,7 @@ for PATHWAY in `cut -f1,15 $FILE  | grep : | cut -f2 | tr \| \\n | cut -d\: -f1 
     do cut -f1,15 $FILE  | grep $PATHWAY: | cut -f1 | sort -u > $OUT.$PATHWAY
 done
 
-echo -e DB\\tproteins\\tgenes
+echo -e DB\\tproteins\\tgenes > $1.stats
 for f in `ls -1 $OUT.*`
     do echo `echo $f | sed 's/^.*ptnas.anot.//'`,`cat $f | wc -l`,`cut -d. -f1 $f | sort -u | wc -l`
 done |  sort -t, -nk3 | tr , \\t > $1.stats
