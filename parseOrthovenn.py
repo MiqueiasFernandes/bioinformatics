9c9dddeab8f72483c06b4f1045c285e35e5c4d12
MiqueiasFernandes <contatomiqueiasfernandes@hotmail.com>
2020-06-06 20:18:51 -0300

    suport filter proteins

--- parseOrthovenn.py
+++ parseOrthovenn.py
@@ -5,20 +5,51 @@
 
 from optparse import OptionParser
 import csv
+from Bio import SeqIO
 
 usage = "usage: %prog -s specie cluster_file.txt"
 parser = OptionParser(usage)
+parser.add_option("-f", "--fasta", dest="fasta", help="Filter proteins from gene/mrna list")
 parser.add_option("-s", "--spec", dest="spec", help="Specie name in onrthovenn cluster file")
 parser.add_option("-g", "--gff", dest="gff", help="gff file to get gene structure", metavar="FILE")
 (options, args) = parser.parse_args()
 
 if len(args) < 1:
-    parser.error('cluster_file.txt is obrigatory!')
-    
-if options.spec is None:
-    parser.error('[-s] Specie arg is mandatory!')
+    parser.error('arg file.txt is obrigatory!')
 
+FILTRO = not options.fasta is None
 GENE = not options.gff is None
+
+gff = []
+mrna2gene = {}
+
+if GENE:
+    gff = [x for x in list(csv.reader(open(options.gff).readlines(), delimiter='\t')) if len(x) > 8 and x[2] == 'mRNA']
+    mrna2gene = {m[8].split('ID=')[1].split(';')[0]: m[8].split('Parent=')[1].split(';')[0] for m in gff}
+
+if FILTRO:
+    names = [l.strip() for l in open(args[0]).readlines() if len(l.strip()) > 0]
+    gs, ms = [], []
+    
+    for k, v in mrna2gene.items():
+        if v in names:
+            gs.append(v)
+            names.append(k)
+            ms.append(k)
+        if k in names:
+            ms.append(k)
+            
+    names = set(names)
+    proteins = [v for k, v in SeqIO.to_dict(SeqIO.parse(options.fasta, 'fasta')).items() if k in names]
+    SeqIO.write(proteins, options.fasta + '-filtered1.faa', 'fasta')
+    for p in proteins:
+        p.id = 'x' + p.id
+    SeqIO.write(proteins, options.fasta + '-filtered2.faa', 'fasta')
+    print('%d proteins writed of %d genes to %d seqs in fasta' % (len(set(ms)), len(set(gs)), len(proteins)))
+    exit()
+
+if options.spec is None:
+    parser.error('[-s] Specie arg is mandatory!')    
 
 data = list(csv.reader(open(args[0]).readlines(), delimiter='\t'))[1:]
 parsed = {}
@@ -31,9 +62,6 @@ for l in data:
     parsed[go] = list(set(mrnas  + parsed[go]))
     
 
-if GENE:
-    gff = [x for x in list(csv.reader(open(options.gff).readlines(), delimiter='\t')) if len(x) == 9 and x[2] == 'mRNA']
-    mrna2gene = {m[8].split('ID=')[1].split(';')[0]: m[8].split('Parent=')[1].split(';')[0] for m in gff}
 
 tba = []
 tbg = []
