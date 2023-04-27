6078f4dc3cb59df9915c0409a1a10badc549cc0e
MiqueiasFernandes <contatomiqueiasfernandes@hotmail.com>
2023-04-27 12:18:24 -0300

    Update ncbi_dataset.sh

--- ncbi_dataset.sh
+++ ncbi_dataset.sh
@@ -7,6 +7,8 @@ FILE="$1"
 ORGANISM="$2"
 CTRL="$3"
 CASE="$4"
+EXP=`echo $ORGANISM | tr -s \  _ `.$CTRL.$CASE
+echo $EXP > experiment.txt
 
 curl -s $REPO/meta_exp.py | python3 - "$FILE" "$ORGANISM" "$CTRL" "$CASE"
 echo wait...
@@ -25,7 +27,7 @@ mv clean_ptnas.faa proteoma.faa
 mv controle smp_ctrl.txt
 mv tratamento smp_case.txt
 rm genes genoma proteoma transcriptoma
-cat smp_ctrl.txt smp_case.txt > `echo $ORGANISM | tr -s \  _ `-$CTRL.$CASE.txt
+cat smp_ctrl.txt smp_case.txt > $EXP.txt
 gzip -k genes.fna proteoma.faa
 
 echo all done.
